594f3eb7fd9c9b317d9df190fa26ddf6c4dc08de
GoMikami <08g0531mkm@gmail.com>
2021-01-12 13:17:34 +0000

    ルート途中

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,13 +1,26 @@
 class BooksController < ApplicationController
-  def index
-  end
 
   def show
   end
-
-  def new
+  
+  def index
+    @books = Book.all
+      def new
+        @books = Book.new
+      end
+  end
+  
+  def create
+    book = Book.new(book_params)
+    book.save
   end
 
   def edit
   end
+  
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+  
 end
