906128caec051cff08008df7750612018e94cd88
GoMikami <08g0531mkm@gmail.com>
2021-01-13 18:18:07 +0000

    微修正残し

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-    get 'books' => 'books#top'
-    get 'books' =>'books#index'
-    post 'books' => 'books#create'
-    get 'books/:id' => 'books#show', as: 'book'
-
+  resources :books
+  root :to => 'books#top'
 end
