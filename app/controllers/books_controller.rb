494d3236440f3f450827fb809b953549b57e190c
GoMikami <08g0531mkm@gmail.com>
2021-01-13 08:22:52 +0000

    投稿の編集画面作成途中

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,12 +1,11 @@
 class BooksController < ApplicationController
 
-  def index
-    @book = Book.all
-    @book = Book.new(book_params)
+  def top
   end
   
-  def show
-    @book = Book.find(params[:id])
+  def index
+    @books = Book.all
+    @book = Book.new
   end
   
   def create
@@ -17,14 +16,25 @@ class BooksController < ApplicationController
           render :new
       end
   end
+  
+  def show
+    @book = Book.find(params[:id])
+  end
 
   def edit
+    @book = Book.find(params[:id])
   end
 
   def update
+    book = Book.find(params[:id])
+    blog.update(blog_params)
+    redirect_to book_path(book)
   end
 
   def destroy
+    book =Book.find(params[:id])
+    book.destroy
+    redirect_to book_path
   end
   
   private
